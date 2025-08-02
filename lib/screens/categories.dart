a5f70e0ad0a380174c5105b4e3b5449082ee6a0a
ChrisLow556 <chrislowxiaohiang@gmail.com>
2025-08-03 03:05:16 +0800

    Each meals screen is now able to display meals based on the category

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,19 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   // Function to switch screeens
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    // where keyword iterates through a list and returns a new iterable based on set conditions
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     // Use Nagivator widget to change between screens by pushing or poping screens
     // Push pushes a new screen to the top of the screen stack
     // Pop removes a screen from the top of the stack
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: 'Some title', meals: []),
+        builder: (ctx) =>
+            MealsScreen(title: category.title, meals: filteredMeals),
       ),
     );
   }
@@ -40,7 +47,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             ),
         ],
