8a111324cbb8d33227ed8ca198672d4c5b6487c4
ChrisLow556 <chrislowxiaohiang@gmail.com>
2025-08-03 02:07:06 +0800

    Created category_grid_item.dart to style and show each category based on dummy_data and added category_grid_item to main.dart

---
+++ lib/screens/categories.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick yo stuff'),
+      ),
+      // Gridview allows you to arrange elements in a grid
+      // crossAxisCount determines the amount of columnes in the grid
+      body: GridView(
+        padding: const EdgeInsets.all(8),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
+      ),
+    );
+  }
+}
